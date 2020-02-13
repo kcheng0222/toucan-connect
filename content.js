ef94d323282b486b76dd12b08917d1f7495ad58c
seancnfg <sf3432@nhusd.k12.ca.us>
2020-02-13 12:53:02 -0800

    Test
    Tests

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-	var images = document.getElementsByTagName('img');
+var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_toucan.jpg';
 images[i].width = 15;
@@ -11,34 +11,35 @@ for(var i = 0; i < images[i].length; i++){
     console.log(images[i]);
 }
 
-var link = document.createElement("link");
-link.href = chrome.extension.getURL("style.css");
-link.type = "text/css";
-link.rel = "stylesheet";
-document.getElementsByTagName("head")[0].appendChild(link);
+// var link = document.createElement("link");
+// link.href = chrome.extension.getURL("styles.css");
+// link.type = "text/css";
+// link.rel = "stylesheet";
+// document.getElementsByTagName("head")[0].appendChild(link);
 
-var color1 = "#F0F0F0"
-
-var color2 = "#ad6602"
+var palette = {
+	headerColor:"#ff0000",
+	subHeaderColor:"#ad6602"
+};
 
 var d = document.getElementsByClassName("darkNeutral");
 var bar = document.getElementsByClassName("mediumHighlight");
 var bar2 = document.querySelectorAll(".displaytbl th");
-console.log("here is d");
-console.log(d);
-//alert(d);
+console.log("here is bar");
+console.log(bar);
+// alert(d);
 for(var i = 0; i < d.length; i++){
 	//console.log(d[i]);
-    d[i].style.backgroundColor = color1;
+    d[i].style.backgroundColor = palette.headerColor;
 }
 
 for(i = 0; i < bar.length; i++){
-    bar[i].style.backgroundColor = color2;
+    bar[i].style.backgroundColor = palette.subHeaderColor;
 }
 for(i = 0; i < bar2.length; i++){
     console.log(bar2[i]);
-    bar2[i].style.backgroundColor = color2;
+    bar2[i].style.backgroundColor = palette.subHeaderColor;
 }
 
-//x.y 
+//x.y
 //x["y"]
