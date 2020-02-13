09813b903c5432e0717afd83def925d7ad0c0500
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2020-02-13 12:52:11 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,44 +1,51 @@
-	var images = document.getElementsByTagName('img');
+var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_toucan.jpg';
-images[i].width = 15;
-images[i].height = 15;
-
+    images[i].src = 'https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_toucan.jpg';
+    images[i].width = 15;
+    images[i].height = 15;
 }
-console.log("here is img");
-console.log(images);
+/*
+//console.log("here is img");
+//console.log(images);
 for(var i = 0; i < images[i].length; i++){
     console.log(images[i]);
 }
+
 
 var link = document.createElement("link");
 link.href = chrome.extension.getURL("style.css");
 link.type = "text/css";
 link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
+*/
 
-var color1 = "#F0F0F0"
+var orange = "#fa8100"
+var color2 = "#fa8100"
 
-var color2 = "#ad6602"
-
-var d = document.getElementsByClassName("darkNeutral");
+var menuChoices = document.getElementsByClassName("darkNeutral");
 var bar = document.getElementsByClassName("mediumHighlight");
 var bar2 = document.querySelectorAll(".displaytbl th");
-console.log("here is d");
-console.log(d);
+
+//console.log("here is d");
+//console.log(d);
 //alert(d);
-for(var i = 0; i < d.length; i++){
-	//console.log(d[i]);
-    d[i].style.backgroundColor = color1;
+for(var i = 0; i < menuChoices.length; i++){
+    //console.log(d[i]);
+    //console.log("editing color");
+    menuChoices[i].style.backgroundColor = orange;
 }
 
 for(i = 0; i < bar.length; i++){
-    bar[i].style.backgroundColor = color2;
+    bar[i].style.backgroundColor = orange;
 }
+console.log("entering");
+//console.log(bar2[0].style);
 for(i = 0; i < bar2.length; i++){
-    console.log(bar2[i]);
-    bar2[i].style.backgroundColor = color2;
+    //console.log("bar2 " + bar2[i]);
+    bar2[i].style.backgroundColor = "#F0000F";
 }
+
+//document.getElementsByClassName("mediumHighlight")[0].style.backgroundColor="#F00000";
 
 //x.y 
 //x["y"]
