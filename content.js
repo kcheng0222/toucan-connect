bc2809e4787f097b8cd8bd0c8dd268987e0b3046
TheCodingCat <prteza@gmail.com>
2020-02-14 19:40:20 -0800

    Fixed css
    Removed the need for a manual timer in javascript by just using the injected styles.css

--- content.js
+++ content.js
@@ -12,35 +12,12 @@ for (var i = 0, l = images.length; i < l; i++) {
     images[i].height = 15;
 }
 
-var palette = {
-    headerColor:"#fe7702",
-    subHeaderColor:"#2b4776",
-    subHeaderColorLight:"#9ac503"
-};
-
-for(menu of document.getElementsByClassName("darkNeutral"))
-    menu.style.backgroundColor = palette.headerColor;
-
-// 1 second is not enough delay.
-var delayInMilliseconds = 1500;
-
-setTimeout(function() { // all the elements that must be styled after the page loads
-  for(foo of document.getElementsByClassName("mediumHighlight"))
-      foo.style.backgroundColor = palette.subHeaderColor;
-
-  for(bar of document.querySelectorAll(".displaytbl th")){
-      console.log(bar);
-      bar.style.backgroundColor = palette.subHeaderColorLight;
-  }
-}, delayInMilliseconds);
-
-
-//put in anon function to wait for page load
+//Anonymous function waits for page to load, just a precautionary measure
 (function(){
     var link = document.querySelector("link[real*='icon']") || document.createElement('link')
     link.type = 'image/x-icon'
     link.rel = 'shortcut icon'
-    link.href = 'http://icontoucan.com/favicon.ico' //found toucan ico
+    link.href = 'http://icontoucan.com/favicon.ico' //Has to be .ico type
     document.getElementsByTagName("head")[0].appendChild(link)
 })()
 
